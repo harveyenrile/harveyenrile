e084bf69f439aaa4a74a1f78a31d02ac3889c4d0
Harvey Enrile <harvey.enrile@gmail.com>
2013-09-25 16:18:39 +0800

    created site with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harveyenrile::Application.config.secret_key_base = '10531cdb74b098e10da5b3a5597df7cd13b38ec9dba8e1e7d9f1078d4a668accb3c3195809428fc9eda368d8d180c219b6aa737bc796f6105bcef7c1ccf45e12'
